ad6fe6d99fd2ca8edb81e074d6c4be2b6ecb2529
Shuaihu <shuaihu.w@gmail.com>
2023-11-14 10:12:22 +0800

    Update default configuration

--- vtq/configuration.py
+++ vtq/configuration.py
@@ -1,11 +1,11 @@
 import dataclasses
-import rate_limit
+from vtq import rate_limit
 
 
 @dataclasses.dataclass
 class Bucket:
-    name: str
-    weight: int
+    name: str = ""
+    weight: int = 100
 
 
 @dataclasses.dataclass
@@ -16,18 +16,18 @@ class RateLimit:
 
 @dataclasses.dataclass
 class VQueueConfiguration:
-    priority: int
-    bucket: Bucket
-    visibility_timeout_seconds: int
+    priority: int = 50
+    bucket: Bucket = dataclasses.field(default_factory=Bucket)
+    visibility_timeout_seconds: int = 3600 * 24
 
 
 class ConfigurationFetcher:
     def __init__(self, workspace="default"):
         self._workspace = workspace
 
-    def configuration_for(vqueue_name: str = "") -> VQueueConfiguration:
+    def configuration_for(self, vqueue_name: str = "") -> VQueueConfiguration:
         """used when adding a new task to the table to specify such as"""
-        pass
+        return VQueueConfiguration()
 
-    def rate_limit_for(vqueue_name: str = "") -> RateLimit:
+    def rate_limit_for(self, vqueue_name: str = "") -> RateLimit:
         pass
