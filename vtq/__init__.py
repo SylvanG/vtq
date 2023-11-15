ae4095793ec177b7579d97e2119be5fcd22990b2
Shuaihu <shuaihu.w@gmail.com>
2023-11-15 20:10:25 +0800

    expose Workspace

--- vtq/__init__.py
+++ vtq/__init__.py
@@ -1,5 +1,6 @@
 from vtq.task_queue import TaskQueue
 from vtq.task import Task
 from vtq.coordinator import Coordinator
+from vtq.workspace import Workspace, WorkspaceFactory
 
-__all__ = ["TaskQueue", "Task", "Coordinator"]
+__all__ = ["TaskQueue", "Task", "Coordinator", "Workspace", "WorkspaceFactory"]
